993412cb93d9feee9d5a4026fe9f7402e4b4d940
Francisco Leiva <franciscoleiva1320@gmail.com>
2026-01-28 23:26:47 -0300

    dia 3: JS interaccon y dark mode basico

--- frontend/js/scrips.js
+++ frontend/js/scrips.js
@@ -1,1 +1,36 @@
 console.log("portafolio cargado correctamente👌");
+const contactBtn= document.querySelector("#contactBtn");
+
+console.log(contactBtn);
+
+const contactSection = document.querySelector("#contactSection");
+
+contactBtn.addEventListener("click", () =>
+{
+    contactSection.classList.toggle("hidden");
+
+    if (contactSection.classList.contains("hidden")){
+        contactBtn.textContent = "Contactame 👋";
+    }
+    else {
+        contactBtn.textContent = "Cerrar ✖️";
+    }
+});
+
+const themeToggle = document.querySelector("#themeToggle");
+
+//cargar preferencia
+if (localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+    themeToggle.textContent = "☀️ Modo claro";
+}
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const theme = document.body.classList.contains("dark")
+        ? "dark"
+        : "light";
+    localStorage.textContent =
+        theme === "dark" ? "☀️ Modo claro" : "🌙 Modo oscuro";
+});
